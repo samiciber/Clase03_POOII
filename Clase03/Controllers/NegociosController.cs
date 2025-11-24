1c08ffecd1c99923a482d9851157228005f98b5f
Soy Samuel <samuelfabisalas@gmail.com>
2025-11-24 13:43:13 -0500

    proyecto de la clase03 terminado

---
+++ Clase03/Controllers/NegociosController.cs
@@ -0,0 +1,44 @@
+﻿using Clase03.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clase03.Controllers
+{
+    public class NegociosController : Controller
+    {
+        IEnumerable<Cliente> clientes()
+        {
+            List<Cliente> temporal = new List<Cliente>();
+            using (SqlConnection cn = new
+                SqlConnection(ConfigurationManager.ConnectionStrings["cadena"].ConnectionString))
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("exec usp_clientes", cn);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    temporal.Add(new Cliente()
+                    {
+                        idcliente = dr.GetInt32(0),
+                        nombrecia = dr.GetString(1),
+                        direccion = dr.GetString(2),
+                        idpais = dr.GetString(3),
+                        fono = dr.GetString(4)
+                    });
+                }
+                dr.Close();
+            }
+            return temporal;
+        }
+        public ActionResult ListaClientes()
+        {
+            return View(clientes());
+        }
+    }
+}
---
+++ Clase03/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Clase03.Models
+{
+    public class Cliente
+    {
+        [Display(Name = "Id Cliente")] public int idcliente { get; set; }
+        [Display(Name = "Nombre Cliente")] public string nombrecia { get; set; }
+        [Display(Name = "Direccion")] public string direccion { get; set; }
+        [Display(Name = "Id Pais")] public string idpais { get; set; }
+        [Display(Name = "Telefono")] public string fono { get; set; }
+
+    }
+}
